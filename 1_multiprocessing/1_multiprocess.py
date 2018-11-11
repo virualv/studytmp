208337ba9072371f123354923c428f53ee0f3abe
virualv <virualv@gmail.com>
2018-11-11 11:07:17 +0800

    motify note --virualv

--- 1_multiprocessing/1_multiprocess.py
+++ 1_multiprocessing/1_multiprocess.py
@@ -1,21 +1,41 @@
 # author:virualv
 #  date :11/6/2018
 
-from multiprocessing import Process
-import time
+# from multiprocessing import Process
+# import time
+#
+# def f(n):
+#     time.sleep(0.5)
+#     print('hello',n,time.ctime())
+#
+# if __name__ == '__main__':
+#     p_list = []
+#
+#     for p in range(3):
+#         p = Process(target=f,args=('xiaomi',))
+#         p_list.append(p)
+#         p.start()
+#
+#     for p in p_list:
+#         p.join()
+#     print('ending')
 
-def f(n):
-    time.sleep(0.5)
-    print('hello',n,time.ctime())
+import time,multiprocessing
+
+class MyProcess(multiprocessing.Process):
+    def __init__(self):
+        super(MyProcess,self).__init__()
+        # self.name = 'process'
+    def run(self):
+        time.sleep(1)
+        print('okay',self.name,time.ctime())
 
 if __name__ == '__main__':
-    p_list = []
-
-    for p in range(3):
-        p = Process(target=f,args=('xiaomi',))
-        p_list.append(p)
-        p.start()
-
-    for p in p_list:
-        p.join()
-    print('ending')
+    t_list = []
+    for i in range(25):
+        t = MyProcess()
+        t_list.append(t)
+    for t in t_list:
+        t.start()
+    for t in t_list:
+        t.join()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# studytmp
-Study Temple
+#studytmp
+####Study Temple
+    本项目仅为方便自己在家和学校学习时，方便同步代码所建。
